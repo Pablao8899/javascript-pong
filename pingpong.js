3dbda62671baa2253bd95da32fd34e799c789bf5
Pablao8899 <melo.oliveira.pablo@escola.pr.gov.br>
2024-08-01 14:06:47 -0300

    amo eu

--- pingpong.js
+++ pingpong.js
@@ -1,9 +1,50 @@
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
+//config velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
 }
 
 function draw(){
-//aqui vou por a cor da "mesa"
-barckground(255,255,255);
+    //função responsável pelo "desenho e animação da mesa"
+    //aqui vou por a cor da "mesa"
+    //A cor da mesa é em rgb
+    background(128,128,0); 
+    //chamando a função cria bolinha para criar a bolinha
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
+    //chamando a função mov bolinha
+    moveBolinha();
+    //chamando a borda
+    Borda();
+}
+
+//função bolinha
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
+}
+
+//função move bolinha
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
+}
+
+function Borda(){
+    if (xBolinha > width || xBolinha < 0){
+        xvelocidadeBolinha *= -1;
+    }
+    if (yBolinha > height || yBolinha < 0){
+        yvelocidadeBolinha *= -1;
+    }
+}
+//função cria raquete
+function criaraquete(){
+    
 }
